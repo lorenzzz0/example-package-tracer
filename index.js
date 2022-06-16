8eeed6386a803cd4d1de516ae7d7949eb6600bb2
lorenzzz0 <lorenzzz0@icloud.com>
2022-06-16 15:05:04 -0500

    update

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ class Tracer {
   constructor(Xray) {
     this.Xray = Xray
   }
+
   createSegment(name, traceId, segmentId){
     return new this.Xray.Segment(name, traceId, segmentId);
   }
+
   setSegment(segment){
-    this.Xray.setSegment(segment);
+    this.Xray.setSegment(segment)
   }
+
   getSegment(segment){
     return this.Xray.getSegment(segment);
-  }
-  createTrace() {
-    console.log('Should create a trace');
   }
 }
 
